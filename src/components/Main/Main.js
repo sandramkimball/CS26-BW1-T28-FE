4a341a682a3a60267d5c9564dbd10cd2c6cb80ac
umbur <you@example.com>
2020-03-04 10:08:58 -0700

    deployed to the zeit

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Main() {
-  return <div>The main page content should be here... it works!</div>;
+  return (
+    <div>
+      The main page(second deployment) content should be here... it works!
+    </div>
+  );
 }
 
 export default Main;
