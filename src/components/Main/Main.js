446704dd0d24100aecce350b216ae7e87e6dd79c
sandramkimball <53502963+sandramkimball@users.noreply.github.com>
2020-03-05 21:46:35 -0800

    created test room div that will be used once room api comes through to populate a map

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -4,27 +4,32 @@ import "./main.css";
 
 
 function Main() {
-  const [directions, setDirection] = useState('');
   const [marsMap, setMarsMap] = useState(null);
 
   const initiateGame = e => {
     e.preventDefault();
     return axiosWithAuth()
-      .get("api/adv/init")
+      .get("api/adv/chambers")
       .then(res => {
-        console.log('gOT mAIL', res)
-        setMarsMap(res.data.chambers)
+        console.log('MAIL CALL', res)
       })
       .catch(err => {
         console.log(err);
       });
   };
 
-
   return (
     <div>
       <h1>Welcome to Mars Underground</h1>
       <img src='https://i.pinimg.com/originals/fe/2c/64/fe2c646744bf4b17d310aed8240aedb3.png'/>
+
+      <div className='map_display'>
+        <p>Test Room</p>
+        {/* {marsMap.forEach(chamber=> (
+           <p>{chamber.title}</p>
+        ))} */}
+      </div>
+
     </div>
   );
 }
